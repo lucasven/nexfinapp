efbe941c62951a68b9ea10379cb6e18138e8745b
Lucas Venturella <lucas.venturella@hotmail.com>
2025-11-26 09:45:36 -0300

    ci tests

--- whatsapp-bot/jest.config.js
+++ whatsapp-bot/jest.config.js
@@ -3,6 +3,7 @@ export default {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.test.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
